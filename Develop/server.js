545d0e396e4256de9ca9295525ab9ceebb54592b
Proton-8 <r.a.shutter@gmail.com>
2021-09-14 05:18:03 -0700

    server sync update

--- Develop/server.js
+++ Develop/server.js
@@ -6,11 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('App listening on port ${PORT}!'));
 });
