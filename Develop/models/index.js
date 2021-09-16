397c7c255bcd04e3d96224ff70d520f50b4a13f2
Proton-8 <r.a.shutter@gmail.com>
2021-09-15 21:06:40 -0700

    model index fix

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -15,7 +15,7 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-Category.belongsToMany(Product, {
+Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
